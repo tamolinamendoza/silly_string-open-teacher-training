1abed34d28f7747d773dcd67cee1bd788bab41c9
vicfriedman <victoria.m.friedman@gmail.com>
2014-05-14 21:32:06 -0400

    cleared out answers

--- silly_string.rb
+++ silly_string.rb
@@ -4,26 +4,26 @@
 #1:
 def my_string
   #code your string here right after the puts
-  puts "placate"
+  puts
 end
 
 #2:
 def capitalize_my_string
   #code your solution here right after the puts
-  puts "placate".upcase
+  puts
 end
 
 #3:
 def downcase_my_string
   #you'll have to manually type your string in all caps first
   #code your solution here right after the puts
-  puts "PLACATE".downcase
+  puts
 end
 
 #4:
 def count_letters
   #code your solution here right after the puts
-  puts "placate".count("placate")
+  puts 
 end
 
 #5: 
@@ -34,11 +34,13 @@ end
 
 def capitalize_first_letter
   #code your solution here right after the puts
-  puts "to placate means to make someone less angry.".capitalize 
+  #you'll need to retype the entire string again. make sure it's not capitalized at all!
+  #that's the point of the lab :)
+  puts
 
 end
 
 def split_my_string
   #code your solution here
-  puts "to placate means to make someone less angry.".split(" ")
+  puts 
 end
